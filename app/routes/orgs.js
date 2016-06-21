bdacf9c0b53b3a58e1c78a73e1d12cad74403a17
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-06-20 17:19:10 -0700

    Add 'favorites' service

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+	favorites: Ember.inject.service();
+
+	model() {
+		return [
+			{ id: 'emberjs' },
+			{ id: 'ember-cli' },
+			{ id: 'microsoft' },
+			{ id: 'yahoo' },
+			{ id: 'netflix' },
+			{ id: 'facebook' }
+		]
+	}
 });
